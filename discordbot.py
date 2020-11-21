f5e9e6b94ea045837601a9942d563c9fdcb45a83
rukoruko9 <66121425+rukoruko9@users.noreply.github.com>
2020-11-21 22:24:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,12 +12,67 @@ from bs4 import BeautifulSoup
 token = os.environ['DISCORD_BOT_TOKEN']
 
 bot = commands.Bot(command_prefix = ".a ")
+@bot.command()
+async def clear(ctx, amount = 5):
+    await ctx.channel.purge(limit = amount)
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="素晴らしいボット", description="天中の情報を提供してくれる、素晴らしいボット", color=0xeee657)
+
+    
+    embed.add_field(name="製作者", value="rukoruko")
+
+    
+    embed.add_field(name="このボットの鯖加入数", value=f"{len(bot.guilds)}")
+    await ctx.send(embed=embed)
+
+@bot.command(aliases=['8ball','eightball','8ボール'])
+async def _8ball(ctx, *, question):
+    responses = ["As I see it, yes.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don’t count on it.",
+                "It is certain.",
+                "It is decidedly so.",
+                "Most likely.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Outlook good.",
+                "Reply hazy, try again.",
+                "Signs point to yes.",
+                "Very doubtful.",
+                "Without a doubt.",
+                "Yes.",
+                "Yes – definitely.",
+                "You may rely on it."]
+    await ctx.send(f'質問: {question};\n答え: {random.choice(responses)}')
 
 
 
 
 
 
+
+@bot.command()
+async def tannto(ctx, *, day):
+    
+    if day == "Mon":
+        await ctx.send('三年女子が担当です')
+    if day == "Tue":
+        await ctx.send("三年男子が担当です")
+    if day == "Wed":
+        await ctx.send("一年が担当です")
+    if day == "Thu":
+        await ctx.send("二年女子が担当です")
+    if day == "Fri":
+        await ctx.send("二年男子が担当です")
+    if day == "Sat":
+        await ctx.send("二年二名と三年二名が担当です")
+    if day == "Sun":
+        await ctx.send("今日は報道の仕事がありません")
 @bot.command()
 async def voca(ctx, word):
     result = requests.get("https://dictionary.goo.ne.jp/word/"+word+"/")
@@ -34,4 +89,4 @@ async def on_command_error(ctx, error):
 
 
 
-bot.run(TOKEN)  
+bot.run(token)  
