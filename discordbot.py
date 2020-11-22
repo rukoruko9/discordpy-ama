879f0db84acbd3bfd17588db64b8c6fe28d1ee08
rukoruko9 <66121425+rukoruko9@users.noreply.github.com>
2020-11-22 13:55:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,37 +1,11 @@
-import traceback
-import os
-import discord
-import random
+  
 from discord.ext import commands
-import requests
-from bs4 import BeautifulSoup
-
+import os
+import traceback
 
 bot =  commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-@bot.command(aliases=['8ball','eightball','8ボール'])
-async def _8ball(ctx, *, question):
-    responses = ["As I see it, yes.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don’t count on it.",
-                "It is certain.",
-                "It is decidedly so.",
-                "Most likely.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Outlook good.",
-                "Reply hazy, try again.",
-                "Signs point to yes.",
-                "Very doubtful.",
-                "Without a doubt.",
-                "Yes.",
-                "Yes – definitely.",
-                "You may rely on it."]
-    await ctx.send(f'質問: {question};\n答え: {random.choice(responses)}')
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -39,6 +13,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 bot.run(token)
