a202409f20b6b4afa4fc5663968ee0ff9cbcefdd
rukoruko9 <66121425+rukoruko9@users.noreply.github.com>
2020-11-22 13:48:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,15 +10,30 @@ from bs4 import BeautifulSoup
 bot = commands.Bot(command_prefix=".a ")
 
 token = os.environ['DISCORD_BOT_TOKEN']
+@bot.command(aliases=['8ball','eightball','8ボール'])
+async def _8ball(ctx, *, question):
+    responses = ["As I see it, yes.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don’t count on it.",
+                "It is certain.",
+                "It is decidedly so.",
+                "Most likely.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Outlook good.",
+                "Reply hazy, try again.",
+                "Signs point to yes.",
+                "Very doubtful.",
+                "Without a doubt.",
+                "Yes.",
+                "Yes – definitely.",
+                "You may rely on it."]
+    await ctx.send(f'質問: {question};\n答え: {random.choice(responses)}')
 
-@bot.command()
-async def voca(ctx, word):
-    result = requests.get("https://dictionary.goo.ne.jp/word/" + word + "/")
-    result = BeautifulSoup(result.text, "html.parser")
-    for meta_tag in result.find_all('meta', attrs={'name': 'personal_snippet'}):
-        await ctx.send(meta_tag.get('content'))
-
-git commit -m "By Yamada Taro" || echo "No changes to commit"
 
 
 bot.run(token)
