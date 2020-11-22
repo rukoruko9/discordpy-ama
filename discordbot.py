f209f42bd43e6774ff1c58c37127f766607fbde7
rukoruko9 <66121425+rukoruko9@users.noreply.github.com>
2020-11-22 13:25:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,4 +56,4 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-bot.run(token)  
+bot.run(token)
